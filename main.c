ad63b75a3380987eabab79897b544dcc2ebc7b39
midjjnfn <mounaram.bis@gmail.com>
2023-12-30 12:59:38 +0100

    adding sdl_ttf

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
-#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 int main(int argc, char* argv[]) {
     SDL_Window* window = NULL;
